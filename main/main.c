a218a77f4d0c675a963c77ca0bed42b1d9868a2b
Peter Bägel (DF5EQ) <peter@baegel.de>
2022-12-22 19:11:13 +0100

    initial commit

---
+++ main/main.c
@@ -0,0 +1,27 @@
+/* ===== file header ===== */
+
+/* ===== includes ===== */
+#include <msp430.h>
+
+/* ===== private datatypes ===== */
+
+/* ===== private symbols ===== */
+
+/* ===== private constants ===== */
+
+/* ===== public constants ===== */
+
+/* ===== private variables ===== */
+
+/* ===== public variables ===== */
+
+/* ===== private functions ===== */
+
+/* ===== public functions ===== */
+
+int main(void)
+{
+    while (1)
+    {
+    }
+}
